cc6a9928c762f8d8cac2262f927fdf4145c20537
epsilon-0 <mathgeniusabhinav@gmail.com>
2016-12-18 16:50:38 -0500

    working code for integration with unity end, need to get networking done

---
+++ Assets/LeapMotion/Scripts/Integration.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Leap;
+using UnityEngine;
+
+namespace LeapmotionIntegration
+{
+    public class ConsolidatedController : Controller
+    {
+        List<Controller> _listOfControllers;
+        List<Frame> _listOfFrames;
+        long _numberOfFrames = 0;
+
+		public static void Main()
+		{
+            Image img = new Image();
+            Console.WriteLine(img.IsValid);
+		}
+
+        public ConsolidatedController(List<Controller> listOfControllers)
+        {
+            _listOfControllers = listOfControllers;
+            _listOfFrames = new List<Frame>();
+        }
+
+        public ConsolidatedController()
+        {
+            return;
+        }
+
+        public ConsolidatedController(int connectionKey)
+        {
+            return;
+        }
+
+        Hand changeHandID(Hand hand, long frameID, int handID)
+        {
+            Hand newHand = new Hand(frameID,
+                                    handID,
+                                    hand.Confidence,
+                                    hand.GrabStrength,
+                                    hand.GrabAngle,
+                                    hand.PinchStrength,
+                                    hand.PinchDistance,
+                                    hand.PalmWidth,
+                                    hand.IsLeft,
+                                    hand.TimeVisible,
+                                    hand.Arm,
+                                    hand.Fingers,
+                                    hand.PalmPosition,
+                                    hand.StabilizedPalmPosition,
+                                    hand.PalmVelocity,
+                                    hand.PalmNormal,
+                                    hand.Direction,
+                                    hand.WristPosition);
+            return newHand;
+        }
+
+        // Merge frames from a list of frames
+        public Frame mergeFrames(List<Frame> listOfFrames, long frameID)
+        {
+
+            List<Hand> listOfHands = new List<Hand>();
+            for(int i = 0; i < listOfFrames.Count; i++)
+            {
+                for(int j = 0; j < listOfFrames[i].Hands.Count; j++)
+                {
+                    int newHandID = listOfFrames.Count * listOfFrames[i].Hands[j].Id + i;
+                    Hand newHand = changeHandID(listOfFrames[i].Hands[j], frameID, newHandID);
+                    listOfHands.Add(newHand);
+                }
+            }
+
+            float minX = 10000000, minY = 10000000, minZ = 10000000, maxX = -1, maxY = -1, maxZ = -1;
+            for(int i = 0; i < listOfFrames.Count; i++)
+            {
+                minX = Math.Min(minX, listOfFrames[i].InteractionBox.Center.x
+                                 - listOfFrames[i].InteractionBox.Width / 2);
+                minY = Math.Min(minY, listOfFrames[i].InteractionBox.Center.y
+                                 - listOfFrames[i].InteractionBox.Height / 2);
+                minZ = Math.Min(minZ, listOfFrames[i].InteractionBox.Center.z
+                                 - listOfFrames[i].InteractionBox.Depth / 2);
+                maxX = Math.Max(maxX, listOfFrames[i].InteractionBox.Center.x
+                                 - listOfFrames[i].InteractionBox.Width / 2);
+                maxY = Math.Max(maxY, listOfFrames[i].InteractionBox.Center.y
+                                 - listOfFrames[i].InteractionBox.Height / 2);
+                maxZ = Math.Max(maxZ, listOfFrames[i].InteractionBox.Center.z
+                                 - listOfFrames[i].InteractionBox.Depth / 2);
+            }
+
+            Vector newSize = new Vector(maxX - minX, maxY - minY, maxZ - minZ);
+            InteractionBox newBox = new InteractionBox(listOfFrames[0].InteractionBox.Center, newSize);
+
+            long newTimeStamp = listOfFrames[0].Timestamp;
+            for(int i = 1; i < listOfFrames.Count; i++)
+            {
+                newTimeStamp = Math.Max(newTimeStamp, listOfFrames[i].Timestamp);
+            }
+
+            Frame mergedFrame = new Frame(frameID,
+                                          newTimeStamp,
+                                          listOfFrames[0].CurrentFramesPerSecond,
+                                          newBox,
+                                          listOfHands);
+            return mergedFrame;
+        }
+
+        // Need to implement all 16 functions for the controller in consolidated form
+
+        public new Frame Frame()
+        {
+            Debug.Log("Called CC.Frame()");
+            List<Frame> currentFrames = new List<Frame>();
+            for (int i = 0; i < _listOfControllers.Count; i++)
+            {
+                if(i == 0)
+                {
+                    currentFrames.Add(_listOfControllers[i].Frame());
+                }
+                else
+                {
+                    currentFrames.Add(((ReceiverController) _listOfControllers[i]).Frame());
+                }
+            }
+            Frame newFrame = mergeFrames(currentFrames, _numberOfFrames);
+            _numberOfFrames += 1;
+            _listOfFrames.Add(newFrame);
+            while(_listOfFrames.Count > 100)
+            {
+                _listOfFrames.RemoveAt(0);
+            }
+            Debug.Log("Number of hands = " + newFrame.Hands.Count);
+            return newFrame;
+        }
+
+        public new void Frame(Frame toFill)
+        {
+            toFill = Frame();
+        }
+
+        public new Frame Frame(int history)
+        {
+            return _listOfFrames[_listOfFrames.Count - history - 1];
+        }
+
+        public new void Frame(Frame toFill, int history)
+        {
+            toFill = _listOfFrames[_listOfFrames.Count - history - 1];
+        }
+
+        public long FrameTimestamp(int history = 0)
+        {
+            return _listOfFrames[_listOfFrames.Count - history - 1].Timestamp;
+        }
+
+        public new bool IsPolicySet(Controller.PolicyFlag policy)
+        {
+            bool isPolicySet = true;
+            for (int i = 0; i < _listOfControllers.Count; i++)
+            {
+                isPolicySet &= _listOfControllers[i].IsPolicySet(policy);
+            }
+
+            return isPolicySet;
+        }
+
+        public new void ClearPolicy(Controller.PolicyFlag policy)
+        {
+            for (int i = 0; i < _listOfControllers.Count; i++)
+            {
+                _listOfControllers[i].ClearPolicy(policy);
+            }
+        }
+
+        public new void SetPolicy(Controller.PolicyFlag policy)
+        {
+            for (int i = 0; i < _listOfControllers.Count; i++)
+            {
+                _listOfControllers[i].SetPolicy(policy);
+            }
+            return;
+        }
+
+        public new void StartConnection()
+        {
+            for (int i = 0; i < _listOfControllers.Count; i++)
+            {
+                _listOfControllers[i].StartConnection();
+            }
+            return;
+        }
+
+        public new void StopConnection()
+        {
+            for (int i = 0; i < _listOfControllers.Count; i++)
+            {
+                _listOfControllers[i].StopConnection();
+            }
+            return;
+        }
+
+        public new long Now()
+        {
+            return _listOfControllers[0].Now();
+        }
+
+        public new FailedDeviceList FailedDevices()
+        {
+            FailedDeviceList lst = new FailedDeviceList();
+            for(int i = 0; i < _listOfControllers.Count; i++)
+            {
+              lst.Concat(_listOfControllers[i].FailedDevices());
+            }
+            return lst;
+        }
+
+        // is an invalid image, maybe need to handle it at some point
+        public new Image RequestImages(Int64 frameID, Image.ImageType type)
+        {
+            return new Image();
+        }
+
+        // same problem as above
+        public new Image RequestImages(Int64 frameID, Image.ImageType type, byte[] imageBuffer)
+        {
+            return new Image();
+        }
+
+        public new Frame GetTransformedFrame(LeapTransform transform, int history = 0)
+        {
+            return Frame(history).TransformedCopy(transform);
+        }
+
+        public new Frame GetInterpolatedFrame(Int64 time)
+        {
+            long closestTimestamp = _listOfFrames[0].Timestamp;
+            int closestFrameIndex = 0;
+            for(int i = 1; i < _listOfFrames.Count; i++)
+            {
+                if(Math.Abs(closestTimestamp - time) > Math.Abs(_listOfFrames[i].Timestamp - time))
+                {
+                    closestTimestamp = _listOfFrames[i].Timestamp;
+                    closestFrameIndex = i;
+                }
+            }
+            return _listOfFrames[closestFrameIndex];
+        }
+    }    
+}
---
+++ Assets/LeapMotion/Scripts/ReceiverController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using Leap;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace LeapmotionIntegration
+{
+    class ReceiverController : Controller
+    {
+        TcpClient conn;
+        Stream stream;
+        StreamWriter w;
+        StreamReader r;
+
+        public ReceiverController(String ip, int port)
+        {
+            conn = new TcpClient();
+            conn.ReceiveTimeout = 100;
+            conn.SendTimeout = 100;
+            conn.Connect(ip, port);
+            stream = conn.GetStream();
+            w = new StreamWriter(stream, Encoding.UTF8);
+            r = new StreamReader(stream);
+        }
+
+        ~ReceiverController()
+        {
+            w.Close();
+            r.Close();
+            stream.Close();
+            conn.Close();
+        }
+
+        public void sendMessage(String message)
+        {
+            try
+            {
+                w.WriteLine(message);
+                w.Flush();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //TcpClient 
+        public new Frame Frame()
+        {
+            sendMessage("Frame");
+            XmlSerializer mySerializer = new XmlSerializer(typeof(Frame));
+            return (Frame)mySerializer.Deserialize(stream);
+        }
+
+        public new bool IsPolicySet(Controller.PolicyFlag policy)
+        {
+            sendMessage("IsPolicySet");
+              
+            XmlSerializer mySerializer = new XmlSerializer(typeof(Controller.PolicyFlag));
+            
+            mySerializer.Serialize(w, policy);
+            return Boolean.Parse(r.ReadLine());
+        }
+
+        public new void SetPolicy(Controller.PolicyFlag policy)
+        {
+            sendMessage("SetPolicy");
+             
+            XmlSerializer mySerializer = new XmlSerializer(typeof(Controller.PolicyFlag));
+            mySerializer.Serialize(w, policy);
+        }
+
+        public new void ClearPolicy(Controller.PolicyFlag policy)
+        {
+            sendMessage("ClearPolicy");
+        
+            XmlSerializer mySerializer = new XmlSerializer(typeof(Controller.PolicyFlag));
+            mySerializer.Serialize(w, policy);
+        }
+
+        public new void StartConnection()
+        {
+            sendMessage("StartConnection");
+        }
+
+        public new void StopConnection()
+        {
+            sendMessage("StopConnection");
+        }
+
+        public new long Now()
+        {
+            sendMessage("Now");
+            return Convert.ToInt64(r.ReadLine());
+        }
+    }
+}
